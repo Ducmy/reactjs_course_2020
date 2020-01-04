42063bb193ffddab4b98839c915894a231b4c6b6
Nguyen Duc My <nguyenducmy393@gmail.com>
2020-01-05 02:42:04 +0700

    Remove some warning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super();
     this.state = {
